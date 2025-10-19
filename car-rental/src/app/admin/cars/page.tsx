4fe7508e011553de23b3cf945172b618567d02f9
Dev Raj <dr395108@gmail.com>
2025-10-19 22:02:26 +0530

    added admin-dashboard

---
+++ car-rental/src/app/admin/cars/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cars = () => {
+  return (
+    <div>
+      <h1 className='text-center text-xl md:text-2xl font-bold text-red-400'>
+      Available Cars</h1>
+
+    </div>
+  )
+}
+
+export default Cars
--- car-rental/src/app/admin/dashboard/page.tsx
+++ car-rental/src/app/admin/dashboard/page.tsx
@@ -1,11 +1,41 @@
 "use client"
-import { signOut } from 'next-auth/react'
+import { CalendarCheck, Car, IndianRupee, Users } from 'lucide-react'
 import React from 'react'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard
-      <button onClick={()=>signOut({callbackUrl:"/sign-in"})}>Logout</button>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 '>
+      <div className="flex flex-col items-center justify-center border shadow-md shadow-gray-400 rounded-xl p-6 hover:shadow-lg transition-all">
+        <Users className="w-8 h-8 text-indigo-600 mb-2" />
+        <h1 className="text-lg md:text-xl font-semibold text-gray-800">
+          Total Users:
+          <span className="text-red-500 ml-1 font-bold">400</span>
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center border shadow-md shadow-gray-400 rounded-xl p-6 hover:shadow-lg transition-all">
+        <Car className="w-8 h-8 text-indigo-600 mb-2" />
+        <h1 className="text-lg md:text-xl font-semibold text-gray-800">
+          Available Cars:
+          <span className="text-red-500 ml-1 font-bold">400</span>
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center border shadow-md shadow-gray-400 rounded-xl p-6 hover:shadow-lg transition-all">
+        <CalendarCheck className="w-8 h-8 text-indigo-600 mb-2" />
+        <h1 className="text-lg md:text-xl font-semibold text-gray-800">
+          Total Bookings:
+          <span className="text-red-500 ml-1 font-bold">400</span>
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center border shadow-md shadow-gray-400 rounded-xl p-6 hover:shadow-lg transition-all">
+        <IndianRupee className="w-8 h-8 text-indigo-600 mb-2" />
+        <h1 className="text-lg md:text-xl font-semibold text-gray-800">
+          Total Revenue:
+          <span className="text-red-500 ml-1 font-bold">₹400</span>
+        </h1>
+      </div>
     </div>
   )
 }
