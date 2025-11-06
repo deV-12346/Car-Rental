bf6dc2a5c0ba44651af294378f69459b2507a16b
Dev Raj <dr395108@gmail.com>
2025-11-06 21:02:22 +0530

    Added get cars api

---
+++ car-rental/src/app/api/car/getCar/route.ts
@@ -0,0 +1,26 @@
+import { connectDb } from "@/libs/connectDb";
+import { CarModel } from "@/model/car.model";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+      await connectDb()
+      try {
+            const cars = await CarModel.find()
+            if(!cars){
+                  return NextResponse.json({
+                        success:true,
+                        message:"Car not found"
+                  },{status:404})
+            }
+            return NextResponse.json({
+                  success:true,
+                  cars
+            })
+      } catch (error) {
+            console.log(error)
+            return NextResponse.json({
+                  success:false,
+                  message:"Something went wrong"
+            },{status:500})
+      }
+}
